0450605e6adf214cb9fb950fabebeceeb6830453
BoatGoate <duynguyen8871@gmail.com>
2025-05-29 18:02:58 -0700

    Saving current work before branching for OAuth

--- Frontend/welcome.js
+++ Frontend/welcome.js
@@ -65,3 +65,28 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+function signIn() {
+    oauth2Endpoint = 'https:accounts.google.com/o/oauth2/v2/auth';
+    let form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', oauth2Endpoint);
+// Lk_rQ_ivlOLwz15gofLXowB7WIa1
+    let params = {
+        "client_id": "644111798598-uru8seebm860hv8dbdhh8vpivtgmldg2.apps.googleusercontent.com",
+        "redirect_uri": "http://127.0.0.1:5501/Frontend/games.html",
+        "response_type": "token",
+        "scope": "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
+        "state": "pass-through_value"
+    }
+
+    for (var p in params) {
+        let input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
